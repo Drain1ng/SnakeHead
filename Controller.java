abc32ee0b3cab6e65f9d670b726307298cd66c2f
Thomas Poulsen <s235218@dtu.dk>
2024-01-09 13:50:11 +0100

    bug fix

--- Controller.java
+++ Controller.java
@@ -14,40 +14,45 @@ public class Controller {
 
     public void handleKeyPress(KeyEvent event) {
         int deg = 0;
-        switch (event.getCode()) {
-            case UP, W: 
-                game.setDirIfValid(Direction.UP);
-                deg = 90;
-                break;
-            case DOWN, S: 
-                game.setDirIfValid(Direction.DOWN);
-                deg = 270;
-                break;
-            case LEFT, A: 
-                game.setDirIfValid(Direction.LEFT); 
-                break;
-            case RIGHT, D: 
-                game.setDirIfValid(Direction.RIGHT);
-                deg = 180; 
-                break;
-            default: return;
-        }
-        game.update();
-        view.updateScore(game.getScore());
-        if (game.isWon()) {
-            view.message(true);
-        } else if(!game.isLost()) {
-            view.updateSnake(deg);
-        } else {
-            view.message(false);
+        try {
+            switch (event.getCode()) {
+                case UP, W:
+                    game.setDir(Direction.UP);
+                    deg = 90;
+                    break;
+                case DOWN, S:
+                    game.setDir(Direction.DOWN);
+                    deg = 270;
+                    break;
+                case LEFT, A:
+                    game.setDir(Direction.LEFT);
+                    break;
+                case RIGHT, D:
+                    game.setDir(Direction.RIGHT);
+                    deg = 180;
+                    break;
+                default: return;
+            }
+            game.update();
+            view.updateScore(game.getScore());
+            if (game.isWon()) {
+                view.message(true);
+            } else if(!game.isLost()) {
+                view.updateSnake(deg);
+            } else {
+                view.message(false);
+            }
+        } catch (IllegalArgumentException e) {
+            // Do nothing
         }
     }
 
-    /* 
+
+    /*
     public void newGameBtnHandle(ActionEvent event) {
         System.out.println(3);
         view.showGameDiff();
     }
     */
-    
+
 }
